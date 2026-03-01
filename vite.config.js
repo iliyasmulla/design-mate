dbd05d720d0ee93e425d5fd60d31b0d9c3ad685d
Iliyas.Mulla <iliyasmullaim1018@gmail.com>
2026-03-01 15:33:45 +0530

    config chnages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/infra-connect/', // <-- important for GitHub Pages
   plugins: [
     tailwindcss(),
     react({
